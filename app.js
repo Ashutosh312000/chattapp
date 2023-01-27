378efaae6b840e03bf29e3e8216227196c537d16
Ashu <ashutosh123asd@gmail.com>
2023-01-27 10:52:40 +0530

    Task 4 Creating Login Screen Frontend

--- app.js
+++ app.js
@@ -18,8 +18,10 @@ const app = express();
 
 
 app.use(cors({
-  origin:"http://123.0.0.1:4000",
-  // methods:['GET','POST'],
+  origin:"http://123.0.0.1:4000", //1) only this origin will be allowed and dont use local host use 124.0.0.1:4000
+  // methods:['GET','POST'], //2) methods tells  ki yeh yeh methods allowed hai, by default all is allowed 
+  //3)optins is a preflight request send to check is asked method is allowed on this origin or not
+  //get ,post dont need options preflight requests where as put, delete etc needs
 }
   
 ));
